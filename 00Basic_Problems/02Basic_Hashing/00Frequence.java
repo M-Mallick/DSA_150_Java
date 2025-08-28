510a539c5319a5ec23c1173cddaa85edbbf55e11
Mridu Mallick <mridumallick101@gmail.com>
2025-08-29 03:50:51 +0530

    Create 00Frequence.java

---
+++ 00Basic_Problems/02Basic_Hashing/00Frequence.java
@@ -0,0 +1,28 @@
+/*
+	Problem Statement: Highest Occurring Element in an Array
+	File Name: 00Frequence.java
+*/
+import java.util.Map;
+import java.util.HashMap;
+
+class Frequence {
+	public  static Map<Integer, Integer> frequence(int[] arr) {
+		Map<Integer, Integer> map = new HashMap<>();
+		for(int i=0; i<arr.length; i++) {
+			int key = arr[i];
+			if(!map.containsKey(key)) {
+				map.put(key,1);
+			}else {
+				map.put(key,map.get(key)+1);
+			}
+		}
+		return map;
+	}
+	public static void main(String[] args) {
+		int[] arr = {1,2,4,1,3,2,4,1};
+		Map<Integer, Integer> freq = frequence(arr);
+		for(Integer key : freq.keySet()) {
+			System.out.println(key+" : "+freq.get(key));
+		}
+	}
+}
