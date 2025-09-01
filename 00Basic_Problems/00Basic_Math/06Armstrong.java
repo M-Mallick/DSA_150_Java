5202237cd270f76c5ae1dcbc10594057f8526b05
Mridu Mallick <mridumallick101@gmail.com>
2025-09-01 22:17:59 +0530

    Add files via upload

---
+++ 00Basic_Problems/00Basic_Math/06Armstrong.java
@@ -0,0 +1,33 @@
+/*
+	Problem Statement: Check if the Number is Armstrong
+	File Name: 06Armstrong.java
+*/
+import java.util.Scanner;
+class Armstrong {
+	private static int count(int num) {
+		int count = 0;
+		while(num != 0) {
+			num /= 10;
+			count++;
+		}
+		return count;
+	}
+	private static boolean armstrong(int num) {
+		int number = num;
+		int count = count(num);
+		int ans = 0;
+		while(num != 0) {
+			int rem = num % 10;
+			ans += (int) Math.pow(rem,count);
+			num /= 10;
+		}
+		return number == ans;
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter Number: ");
+		int num = sc.nextInt();
+		boolean armstrong = armstrong(num);
+		System.out.print(num+" is a Armstrong: "+armstrong);
+	}
+}
