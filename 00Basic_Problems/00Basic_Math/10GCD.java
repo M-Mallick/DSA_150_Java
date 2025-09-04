149b8215b161145ae1b363fa77bc14a1f4084537
Mridu Mallick <mridumallick101@gmail.com>
2025-09-04 11:24:38 +0530

    Add files via upload

---
+++ 00Basic_Problems/00Basic_Math/10GCD.java
@@ -0,0 +1,26 @@
+/*
+	Problem Statement: GCD of Two Numbers
+	File Name: 10GCD.java
+*/
+import java.util.Scanner;
+class GCD {
+	private static int gcd(int num1, int num2) {
+		int gcd = 0;
+		for(int i=1; i<=num1; i++) {
+			if(num1 % i == 0 && num2 % i == 0) {
+				if(gcd < i) {
+					gcd = i;
+				}
+			} 
+		}
+		return gcd;
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter Two number: ");
+		int num1 = sc.nextInt();
+		int num2 = sc.nextInt();
+		int ans = gcd(num1,num2);
+		System.out.print("GCD("+num1+","+num2+"): "+ans);
+	}
+} 
