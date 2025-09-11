4a761f7c28b01fa883381a95d007caa20aa7f712
Mridu Mallick <mridumallick101@gmail.com>
2025-09-11 23:37:56 +0530

    Update and rename temp.js to 00Reverse.java

---
+++ 00Basic_Problems/03Basic String/00Reverse.java
@@ -0,0 +1,27 @@
+/*
+	Problem Statement: Reverse a String
+	File Name: 00Reverse.java
+*/
+class Reverse {
+    public static String reverseString(String s) {
+        // code here
+        char[] str = s.toCharArray();
+        int left = 0;
+        int right = str.length - 1;
+        
+        while(left < right) {
+            char temp = str[left];
+            str[left] = str[right];
+            str[right] = temp;
+            left++;
+            right--;
+        }
+        
+        return new String(str);
+    }
+    public static void main(String[] args) {
+    	String s = "abcdef";
+    	String rev = reverseString(s);
+    	System.out.print(rev);
+    }
+}
