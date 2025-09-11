77258ddd3813d4a29f918f5d805b421ae2af65c4
Mridu Mallick <mridumallick101@gmail.com>
2025-09-11 23:42:53 +0530

    Update temp.js

--- 00Basic_Problems/03Basic String/temp.js
+++ 00Basic_Problems/03Basic String/temp.js
@@ -1,1 +1,26 @@
-aa
+/*
+	Problem Statement: Reverse a String II
+	File Name: 01ReverseII.java
+*/
+class Reverse {
+    public static String reverseString(String s) {
+        // code here
+        char[] str = s.toCharArray();
+        int left = 0;
+        int right = str.length - 1;
+        
+        while(left < right) {
+            char temp = str[left];
+            str[left] = str[right];
+            str[right] = temp;
+            left++;
+            right--;
+        }
+        return new String(str);
+    }
+    public static void main(String[] args) {
+    	String s = "abcdef";
+    	String rev = reverseString(s);
+    	System.out.print(rev);
+    }
+}
