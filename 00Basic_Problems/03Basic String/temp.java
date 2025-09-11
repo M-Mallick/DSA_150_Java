05a515365b5c32f5879c116dfbb4ad9ab50844b1
Mridu Mallick <mridumallick101@gmail.com>
2025-09-11 23:46:38 +0530

    Create temp.java

---
+++ 00Basic_Problems/03Basic String/temp.java
@@ -0,0 +1,15 @@
+/*
+	Problem Statement: Palindrome Check
+	File Name: 02Palindrome.java
+*/
+class Palindrome {
+    public static String palindrome(String num) {
+        // code here
+        
+    }
+    public static void main(String[] args) {
+    	String s = "abcdef";
+    	String rev = reverseString(s);
+    	System.out.print(rev);
+    }
+}
