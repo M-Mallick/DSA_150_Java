eda3a9c529426f17d2ef467bfeb50bf7efbf4127
Mridu Mallick <mridumallick101@gmail.com>
2025-09-26 22:49:46 +0530

    Update temp.java

--- 02Array & Hashing/02Valid Anagram/temp.java
+++ 02Array & Hashing/02Valid Anagram/temp.java
@@ -1,1 +1,19 @@
-..
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) return false;
+        HashMap<Character, Integer> map = new HashMap<>();
+        for(int i=0; i<s.length(); i++) {
+            char key = s.charAt(i);
+            map.put(key, map.getOrDefault(key, 0) + 1);
+        }
+        for(int i=0; i<t.length(); i++) {
+            char key = t.charAt(i);
+            if(map.containsKey(key) && map.get(key) != 0) {
+                map.put(key, map.get(key) - 1);
+            }else {
+                return false;
+            }
+        }
+        return true;
+    }
+}
