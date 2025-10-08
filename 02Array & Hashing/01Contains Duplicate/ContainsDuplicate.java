11d1736f9bd4a84749be9a07f5bef61e52a562fb
Mridu Mallick <mridumallick101@gmail.com>
2025-10-08 23:47:37 +0530

    Update ContainsDuplicate.java

--- 02Array & Hashing/01Contains Duplicate/ContainsDuplicate.java
+++ 02Array & Hashing/01Contains Duplicate/ContainsDuplicate.java
@@ -1,6 +1,7 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
         HashMap<Integer, Integer> map = new HashMap<>();
+
         for(int i=0; i<nums.length; i++) {
             if(!map.containsKey(nums[i]))
                 map.put(nums[i],i);
