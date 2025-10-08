a6520a2a3341111b3659bbd48cdc695c67e0b301
Mridu Mallick <mridumallick101@gmail.com>
2025-10-08 23:48:47 +0530

    Update Concatenation.java

--- 02Array & Hashing/00Concatenation of Array/Concatenation.java
+++ 02Array & Hashing/00Concatenation of Array/Concatenation.java
@@ -2,6 +2,7 @@ class Solution {
     public int[] getConcatenation(int[] nums) {
 
         int[] arr = new int[2*nums.length];
+
         for(int i=0; i<nums.length; i++) {
             arr[i] = nums[i];
             arr[nums.length + i] = nums[i];
