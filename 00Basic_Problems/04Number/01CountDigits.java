2ad1d7e543ce1521c5dbed21e0c80bcf41efa08f
Mridu Mallick <mridumallick101@gmail.com>
2026-02-08 08:52:18 +0530

    Add program to count digits in a number

---
+++ 00Basic_Problems/04Number/01CountDigits.java
@@ -0,0 +1,17 @@
+public class Main {
+	public static void main(String[] args) {
+	    // Count digits in a number
+	    int count = 0;
+	    int num = 0;
+	    if(num == 0) {
+	        count = 1;
+	    }
+	    else {
+	        while(num != 0) {
+	            num /= 10;
+	            count++;
+	        }
+	    }
+		System.out.println("Count : "+count);
+	}
+}
