61a27bb3f5970a14518442910d396fd7de17d6ed
Mridu Mallick <mridumallick101@gmail.com>
2026-02-08 23:42:48 +0530

    Create 05RemoveWhiteSpaces.java

---
+++ 00Basic_Problems/03Basic String/05RemoveWhiteSpaces.java
@@ -0,0 +1,15 @@
+// Remove white spaces from string in java
+class Main {
+    public static void main(String[] args) {
+        String ip = "Hello World"; // It is immutabe
+        
+        // Method 1
+        StringBuilder s = new StringBuilder();
+        for(int i=0; i<ip.length(); i++) {
+            if(ip.charAt(i) != ' ') {
+                s.append(ip.charAt(i));
+            }
+        }
+        System.out.print(s.toString());
+    }
+}
