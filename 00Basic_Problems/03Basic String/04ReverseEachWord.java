b3bac4a14ba537013bfcb5d19eda61fbb0c02aa8
Mridu Mallick <mridumallick101@gmail.com>
2026-02-08 23:14:37 +0530

    Create 04ReverseEachWord.java

---
+++ 00Basic_Problems/03Basic String/04ReverseEachWord.java
@@ -0,0 +1,31 @@
+// Reverse Each word of a string in java
+import java.util.*;
+class Main {
+    private static String reverse(String str) {
+        return new StringBuilder(str).reverse().toString();
+    }
+    public static void main(String[] args) {
+        String ip = "Hello World"; // It is immutabe
+        
+        // Method 1
+        String[] arr = ip.split(" ");
+        for(int i=0; i<arr.length; i++) {
+            arr[i] = reverse(arr[i]);
+        }
+        System.out.print(Arrays.toString(arr));
+        
+        StringBuilder s = new StringBuilder();
+        for(int i=0; i<arr.length; i++) {
+            if(i == arr.length - 1) {
+                s.append(arr[i]);
+            }else {
+                s.append(arr[i]);
+                s.append(" ");
+            }
+        }
+        System.out.print("\nOutput1: "+s.toString());
+        
+        String s1 = String.join(" ", arr);
+        System.out.print("\nOutput2: "+s1);
+    }
+}
