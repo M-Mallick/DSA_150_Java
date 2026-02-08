c95756e575fc8b2d3f15d05e387242c8c6a89d9a
Mridu Mallick <mridumallick101@gmail.com>
2026-02-08 08:46:39 +0530

    Add swap program in Java
    Implement a Java program to swap two numbers without using a third variable.

---
+++ 00Basic_Problems/04Number/00swap.java
@@ -0,0 +1,13 @@
+public class Main {
+	public static void main(String[] args) {
+	    // Swapping two number withot using 3rd variable
+    
+	    int a = 10, b = 8;
+	    
+	    a = a + b;    // a = 18
+	    b = a - b;    // b = 10
+	    a = a - b;    // a = 8
+	    
+		System.out.println("a : "+a+", b : "+b);
+	}
+}
